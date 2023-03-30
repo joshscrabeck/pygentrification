9b389fd59e6cd63948b896893831aa18b48d4b35
Winn <winncostantini@gmail.com>
2023-03-30 15:45:44 -0400

    added psudeocode

---
+++ harmonize_tracts_func.py
@@ -0,0 +1,120 @@
+
+
+import pandas as pd
+import os
+from tobler.area_weighted import area_interpolate
+import geopandas as gpd
+from functools import reduce
+
+
+#os.chdir('/Users/wc555/gus8066/gentrification-indices/bates-freeman-data')
+
+#%%
+
+# ##Create test dfs##
+
+# #Import ACS_2020_tenure.csv and ACS_2010_tenure.csv
+# acs_2020_tenure = pd.read_csv('ACS_2020_tenure.csv', skiprows= [1]) #skip row with column descriptions
+# acs_2010_tenure = pd.read_csv('ACS_2010_tenure.csv', skiprows= [1]) 
+# shp10 = gpd.read_file('/Users/wc555/gus8066/tl_2010_42101_tract10.shp').to_crs('epsg:6565')
+# shp20 = gpd.read_file('tl_2022_42_tract.shp').to_crs('epsg:6565')
+# shp20 = shp20[shp20['COUNTYFP'] == '101']
+
+# #Rename relevant columns and drop unecessary columns
+# acs_2020_tenure = acs_2020_tenure.rename(columns = {'B25003_001E': 'pop_tenure', 'B25003_001M':'popten_e', 'B25003_002E': 'owners', 'B25003_002M':'owner_e', 'B25003_003E': 'renters', 'B25003_003M':'renter_e'})
+# acs_2020_tenure.dropna(axis='columns', inplace = True)
+# acs_2020_tenure = acs_2020_tenure[['GEO_ID', 'NAME', 'pop_tenure', 'owners', 'renters']]
+# acs_2020_tenure['GEO_ID'] = acs_2020_tenure['GEO_ID'].apply(lambda x: x[-11:])
+# shp_merge_2020 = shp20.merge(acs_2020_tenure, left_on = 'GEOID', right_on = 'GEO_ID', suffixes = ('_x', '_y'))
+
+
+# acs_2010_tenure = acs_2010_tenure.rename(columns = {'B25003_001E': 'pop_tenure', 'B25003_001M':'popten_e', 'B25003_002E': 'owners', 'B25003_002M':'owner_e', 'B25003_003E': 'renters', 'B25003_003M':'renter_e'})
+# acs_2010_tenure.dropna(axis='columns', inplace = True)
+# acs_2010_tenure = acs_2010_tenure[['GEO_ID', 'NAME', 'pop_tenure', 'owners', 'renters']]
+# acs_2010_tenure['GEO_ID'] = acs_2010_tenure['GEO_ID'].apply(lambda x: x[-11:])
+# shp_merge_2010 = shp10.merge(acs_2010_tenure, left_on = 'GEOID10', right_on = 'GEO_ID', suffixes = ('_x', '_y'))
+
+
+#%%
+
+def harmonize_tracts(target_df, input_dfs = []):
+    ''' This function takes in a target GeoDataFrame and a list of input GeoDataFrames. The target_df has the target geometry and the function uses the tobler package's areal interpolation
+    function to harmonize the data in each input GeoDataFrame with the geometry in the target dataframe. 
+    
+    Parameters
+    -------------
+    target_df: GeoDataFrame with target geometry
+    input_dfs: List of GeoDataFrames 
+
+    Returns
+    ------------
+    GeoDataFrame
+    
+    This function will return a single GeoDataFrame with all interpolated columns from all input dfs and the geometry and all original columns from the target df. 
+    The interpolated columns will have suffixes corresponding to their order in the input_dfs parameter. If there is more than one input df (for year o - year n), the suffix will start at '_yr0' and the target_df will have teh suffix '_yr{n}'. If there is only one input df the suffix will be '_yr1' and the target_df will have suffix '_yr2'.
+    
+    References
+    -------------
+    https://github.com/pysal/tobler
+    
+    Notes
+    -------------
+    This function uses lists of possible extensive variables and intensive variables, which together include all possible column names needed to calculate the gentrification indices in this package.
+    
+      '''
+   
+    #take in list of input dfs and make two lists of lists - one for extensive and one for intensive
+    
+   
+    all_extensive_vars = ['pop_tenure', 'owners', 'renters', 'pop_edu', 'pop_edu_m', 'ASdeg_m', 'BAdeg_m', 'MAdeg_m', 'profdeg_m', 'drdeg_m', 'pop_edu_f', 'ASdeg_f', 'BAdeg_f', 'MAdeg_f', 'profdeg_f', 'drdeg_f', 'pop_race', 'white','tothouse', 'newhouse_col1', 'newhouse_col2', 'newhouse_col3']
+    all_intensive_vars = ['mfi', 'mhv', 'mhi']
+    
+    extensive_variables = []
+    intensive_variables = []
+    
+    for input_df in input_dfs:
+        e_vars = []
+        i_vars = []
+        col_list = input_df.columns.to_list()
+        for col in col_list:
+            if col in all_extensive_vars:
+                e_vars.append(col)
+            elif col in all_intensive_vars:
+                i_vars.append(col)
+        if len(e_vars) > 0:
+            extensive_variables.append(e_vars)
+        else:
+            extensive_variables.append(None)
+        if len(i_vars) > 0:
+            intensive_variables.append(i_vars)
+        else:
+            intensive_variables.append(None)
+    
+    if len(input_dfs) == 1:
+        i = 1
+    
+    elif len(input_dfs) > 1:
+        i = 0
+    
+    varslist = 0
+    merge_dfs = [target_df]
+    
+    for input_df in input_dfs:    
+        interpolated_columns = area_interpolate(input_df, target_df, extensive_variables = extensive_variables[varslist], intensive_variables= intensive_variables[varslist])
+        interpolated_columns.drop(['geometry'], axis = 1, inplace = True)
+        interpolated_columns = interpolated_columns.add_suffix(f'_yr{i}')
+        merge_dfs.append(interpolated_columns)
+        i += 1
+        varslist += 1
+       
+    
+    output = reduce(lambda left,right: pd.merge(left,right, left_index = True, right_index = True, how='inner'), merge_dfs)
+
+    return output
+
+#%%
+
+#testdf = harmonize_tracts(shp_merge_2020, input_dfs = [shp_merge_2010])
+
+
+
--- python_census_api_script_3_28.py
+++ python_census_api_script_3_28.py
@@ -17,81 +17,119 @@ from pprint import pprint
 import urllib
 import shutil
 import geopandas as gpd
+import warnings
+from tobler.area_weighted import area_interpolate
+from functools import reduce
+from indices_constants import ding_vars, ding_vars_area, bates_vars_acs_yr0, bates_vars_census_yr0, bates_vars_yr1, bates_vars_yr2, bates_vars_area, master_county_dict, master_tract_dict
+from harmonize_tracts_func import harmonize_tracts
 
-HOST = "https://api.census.gov/data"
-year = "2020"
-dataset = "acs/acs5"
-acs_base_url = "/".join([HOST, year, dataset])
+# HOST = "https://api.census.gov/data"
+# year = "2020"
+# dataset = "acs/acs5"
+# acs_base_url = "/".join([HOST, year, dataset])
 
-print(acs_base_url)
+# print(acs_base_url)
 
-HOST = "https://www2.census.gov/geo/tiger/TIGER2020/TRACT"
-dataset = "tl_2020_42_tract.zip"
-tiger_2020_url = "/".join([HOST, dataset])
+# HOST = "https://www2.census.gov/geo/tiger/TIGER2020/TRACT"
+# dataset = "tl_2020_42_tract.zip"
+# tiger_2020_url = "/".join([HOST, dataset])
 
-HOST = "https://api.census.gov/data"
-year = "2010"
-dataset = "acs/acs5"
-acs_base_url_2010 = "/".join([HOST, year, dataset])
-print(acs_base_url_2010)
+# HOST = "https://api.census.gov/data"
+# year = "2010"
+# dataset = "acs/acs5"
+# acs_base_url_2010 = "/".join([HOST, year, dataset])
+# print(acs_base_url_2010)
 
-HOST = "https://api.census.gov/data"
-year = "2000"
-dataset = "dicennial/dicennial"
-census_base_url_2000 = "/".join([HOST, year, dataset])
+# HOST = "https://api.census.gov/data"
+# year = "2000"
+# dataset = "dicennial/dicennial"
+# census_base_url_2000 = "/".join([HOST, year, dataset])
 
-print(census_base_url_2000)
+# print(census_base_url_2000)
 
 
 # In[6]:
 
 
-bates_vars = {'codes':['B25003_001E', 'B25003_001M', 
-                       'B25003_002E', 'B25003_002M', 
-                       'B25003_003E', 'B25003_003M', 
-                       'B03002_001E', 'B03002_001M', 
-                       'B03002_003E', 'B03002_003M', 
-                       'B15002_001E', 'B15002_001M', 'B15002_002E', 
-                       'B15002_002M', 'B15002_014E', 'B15002_014M', 
-                       'B15002_015E', 'B15002_015M', 'B15002_016E', 
-                       'B15002_016M', 'B15002_017E', 'B15002_017M', 
-                       'B15002_018E', 'B15002_018M','B15002_019E', 
-                       'B15002_019M', 'B15002_031E', 'B15002_031M', 
-                       'B15002_032E', 'B15002_032M', 'B15002_033E', 
-                       'B15002_033M', 'B15002_034E', 'B15002_034M', 
-                       'B15002_035E', 'B15002_035M',
-                       'B19113_001E', 'B19113_001M',
-                       'B19013_001E', 'B19013_001M',
-                       'B25077_001E', 'B25077_001M',
-                       'B25034_001E', 'B25034_001M',
-                       'B25034_002E', 'B25034_002M',
-                       'B25034_003E', 'B25034_003M'],
-              'columns': ['pop_ten','pop_ten_e', 'owner', 'owner_e', 'renter', 'renter_e',
-                          'poprac', 'poprac_e', 'white', 'white_e', 'popedu', 'popedu_e',
-                          'popedum', 'popedum_e', 'ASdegm', 'ASdegm_e', 'BAdegm', 'BAdegm_e',
-                          'MAdegm', 'MAdegm_e', 'prodegm', 'prodegm_e', 'drdegm',
-                          'drdegme', 'popeduf', 'popeduf_e', 'ASdegf', 'ASdegf_e',
-                          'BAdegf', 'BAdegf_e', 'MAdegf', 'MAdegf_e', 'prodegf',
-                          'prodegf_e', 'drdegf', 'drdegf_e', 'mfi', 'mfi_e', 'mhi', 'mhi_e',
-                          'mhv', 'mhv_e', 'tothous', 'tothous_e', 'y14topr', 'y14topr_e',
-                          'y10to13', 'y10to13_e',]}
+# bates_vars = {'codes':['B25003_001E', 'B25003_001M', 
+#                        'B25003_002E', 'B25003_002M', 
+#                        'B25003_003E', 'B25003_003M', 
+#                        'B03002_001E', 'B03002_001M', 
+#                        'B03002_003E', 'B03002_003M', 
+#                        'B15002_001E', 'B15002_001M', 'B15002_002E', 
+#                        'B15002_002M', 'B15002_014E', 'B15002_014M', 
+#                        'B15002_015E', 'B15002_015M', 'B15002_016E', 
+#                        'B15002_016M', 'B15002_017E', 'B15002_017M', 
+#                        'B15002_018E', 'B15002_018M','B15002_019E', 
+#                        'B15002_019M', 'B15002_031E', 'B15002_031M', 
+#                        'B15002_032E', 'B15002_032M', 'B15002_033E', 
+#                        'B15002_033M', 'B15002_034E', 'B15002_034M', 
+#                        'B15002_035E', 'B15002_035M',
+#                        'B19113_001E', 'B19113_001M',
+#                        'B19013_001E', 'B19013_001M',
+#                        'B25077_001E', 'B25077_001M',
+#                        'B25034_001E', 'B25034_001M',
+#                        'B25034_002E', 'B25034_002M',
+#                        'B25034_003E', 'B25034_003M'],
+#               'columns': ['pop_ten','pop_ten_e', 'owner', 'owner_e', 'renter', 'renter_e',
+#                           'poprac', 'poprac_e', 'white', 'white_e', 'popedu', 'popedu_e',
+#                           'popedum', 'popedum_e', 'ASdegm', 'ASdegm_e', 'BAdegm', 'BAdegm_e',
+#                           'MAdegm', 'MAdegm_e', 'prodegm', 'prodegm_e', 'drdegm',
+#                           'drdegme', 'popeduf', 'popeduf_e', 'ASdegf', 'ASdegf_e',
+#                           'BAdegf', 'BAdegf_e', 'MAdegf', 'MAdegf_e', 'prodegf',
+#                           'prodegf_e', 'drdegf', 'drdegf_e', 'mfi', 'mfi_e', 'mhi', 'mhi_e',
+#                           'mhv', 'mhv_e', 'tothous', 'tothous_e', 'y14topr', 'y14topr_e',
+#                           'y10to13', 'y10to13_e',]}
 
-test_vars = {'B25064_001E': 'med_rent_1', 'B25077_001E': 'med_home_val_1',
-       'S1501_C01_001E': '18_24_pop_1'}
+# test_vars = {'B25064_001E': 'med_rent_1', 'B25077_001E': 'med_home_val_1',
+#        'S1501_C01_001E': '18_24_pop_1'}
 
-def acs_request(year, state, county, index_vars):
+def acs_request_tract(year, state, county, index_vars):
     HOST = "https://api.census.gov/data"
-    year = "{year}"
+    year = f"{year}"
     dataset = "acs/acs5"
     acs_base_url = "/".join([HOST, year, dataset])
-    print(acs_base_url)
+    #print(acs_base_url)
     predicates = {}
     predicates["get"] = ",".join(index_vars)
     predicates["for"] = "tract:*"
     predicates["in"] = f"state:{state}, county:{county}"
     #predicates["key"] = ____________________
     j = requests.get(acs_base_url, params=predicates).json()
-    df = pd.DataFrame(data=j[1:], columns=j[0]).rename(columns = master_dict, inplace=True)
+    df = pd.DataFrame(data=j[1:], columns=j[0]).rename(columns = master_tract_dict, inplace=True)
+    return df
+
+def acs_request_area(year, state, county, index_vars):
+    HOST = "https://api.census.gov/data"
+    year = "{year}"
+    dataset = "acs/acs5"
+    acs_base_url = "/".join([HOST, year, dataset])
+    #print(acs_base_url)
+    predicates = {}
+    predicates["get"] = ",".join(index_vars)
+    predicates["for"] = f"county:{county}"
+    predicates["in"] = f"state:{state}"
+    #predicates["key"] = ____________________
+    j = requests.get(acs_base_url, params=predicates).json()
+    df = pd.DataFrame(data=j[1:], columns=j[0]).rename(columns = master_tract_dict, inplace=True)
+    return df
+
+# HOST = "https://api.census.gov/data"
+# year = "2000"
+# dataset = "dicennial/dicennial"
+# census_base_url_2000 = "/".join([HOST, year, dataset])
+
+def census_request_tract(year, state, county, index_vars):
+    HOST = "https://api.census.gov/data"
+    year = f"{year}"
+    dataset = "dicennial/dicennial"
+    census_base_url_2000 = "/".join([HOST, year, dataset])
+    predicates = {}
+    predicates["get"] = ",".join(index_vars)
+    predicates["for"] = f"county:{county}"
+    predicates["in"] = f"state:{state}"
+    j = requests.get(census_base_url, params=predicates).json()
+    df = pd.DataFrame(data=j[1:], columns=j[0]).rename(columns = master_tract_dict, inplace=True)
     return df
 
 def tiger_request(year, state, county):
@@ -110,30 +148,124 @@ def merge(acs_df, t_df):
     complete_df=pd.merge(t_df, acs_df)
     return complete_df
 
-def get_api_data(state, county, years, index):
+def get_api_data_tract(state, county, years, indices):
     
+    ###Check input###
     
-    var_codes=[]
-    col_names=[]
+    #Years
     
-    #if index = master:
-        #vars = master_vars
-       
+    #sort years
+    years.sort()
+    
+    no_data_yrs = [2001, 2002, 2003, 2004, 2005]
+    
+    for year in years:
+        if (int(year) in no_data_yrs) or int(year) < 2000: 
+            raise ValueError("This function cannot retrieve data for years before 2000 or from 2001-2005")
+    
+    #Bates errors
+    if ("bates" in indices) and (len(years) < 3):
+        raise Exception("to calculate the Bates-Freeman indices, you must input three years")
         
-    for yr in years:
-        acs_df = acs_request(year, state, county, var_codes, col_names)
-        tig_df = tiger_request(year, state, county)
+    if ("bates" in indices) and ((int(years[1]) - int(years[0]) < 10) or (int(years[2])- int(years[1]))):
+        warnings.warn("The methodology used in Bates(2013) used three study years, each 10 years apart. If you use the resulting df from this function to calculate the Bates-Freeman indices, be aware that are using a different interval between study years.")
+        
     
-             
+    ###Set up varcodes###
+    
+    
+    #Initialize empty list for yr 1 and 2 varcodes
+    var_codes_yr1=[]
+    var_codes_yr2=[]
+    
+    
+    #loop through indices list
+        for index in indices:
+        
+        #if ding
+        
+            #for loop through ding_vars
+                #append item in list to var_codes_yr_1
+                #append item in list to var_codes_yr_2
+    
+    
+        #if bates
+        if index == "bates":
+            for varcode in bates_vars_yr1:
+                var_codes.append(varcode)
+            for varcode in bates_vars_yr2:
+                var_codes.append(varcode)
+            
+            if int(years[0]) == 2000:
+                var_codes_yr0 = bates_vars_census_yr0    
+                    
+            else:
+                var_codes_yr0 = bates_vars_acs_yr0
+                    
+        
+        #drop duplicates
+        var_codes_yr1 = list(set(var_codes_yr1))
+        var_codes_yr2 = list(set(var_codes_yr2))
+                
+        
+    ###api calls###
+    
+    #initialize empty list df_list
+    
+    ##Year 0##
+    
+    #if len(years) > 2:
+    
+        #if year[0] is 2000
+    
+            #assign census_df_yr0 to output of census_request_tract function for 2000 and var_codes_yr0
+        
+            #assign tiger_df_yr0 to output of tiger_request for 2000
+        
+            #merge and assign to df_yr0
+        
+            #append df_year0 to df_list
+            
+        #else
+        
+            #assign acs_df_yr0 to output of acs_request_tract function for years[0] and var_codes_yr0
+            
+            #assign tiger_df_yr0 to output of tiger_request for years[0]
+        
+            #merge and assign to df_yr0
+        
+            #append df_year0 to df_list
+            
+    ##Year 1##
+        
+        #assign acs_df_yr1 to output of acs_request_tract function for years[1] and var_codes_yr1
+        
+        #assign tiger_df_yr1 to output of tiger_request for years[1]
+    
+        #merge and assign to df_yr1
+    
+        #append df_yr1 to df_list
+        
+    ##Year 2##
+        
+        #assign acs_df_yr2 to output of acs_request_tract function for years[2] and var_codes_yr2
+        
+        #assign tiger_df_yr2 to output of tiger_request for years[2]
+    
+        #merge and assign to df_yr2
+    
+        #append df_yr2 to df_list
+        
+    
+    ###Harmonize dataframes to the tracts in year 2###
+
+    target_df = df_list[-1]
+    input_dfs = df_list[0:-1]
+    
+    df = harmonize_tracts(target_df, input_dfs)
+       
+              
     return df
 
-
-
-
-#### use library urllib will read a data stream (chapter 20 in GIS programming textbook)
-### return a dataframe, (dataframe creation data = r.json()[1:])
     
 
-#use geosnap for 1990-2010, tiger census for 2020 geometries.
-#harmonize.harmonize(gdf[, target_year, ...]) ,
-#Use spatial interpolation to standardize neighborhood boundaries over time.
