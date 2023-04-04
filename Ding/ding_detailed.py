849bfcadc9a02ea2a02695be014b2d6d88a2290c
Adam Thompson <AdamTheThompson93@gmail.com>
2023-04-04 18:14:56 -0400

    ding, api removed and made compatible with other packages in the module

--- Ding/ding_detailed.py
+++ Ding/ding_detailed.py
@@ -5,169 +5,165 @@
 import pandas as pd
 import os
 import numpy as np
-import requests
-
+import geopandas as gpd
+import folium
 
 
 os.getcwd()
 os.chdir('.\documents\gus_8066\gentrification-indices\ding')
 
-df = pd.read_csv('ding_df.csv')
+df_tract = gpd.read_file('C:/Users/tul54884/Documents/gus_8066/gentrification-indices/folium/dummydata.geojson')
 
-#%% The Ding function, more detailed version, starts with defining nested census api calls
 
-def ding_calc( yr1, yr2, study_area, inplace = False):
+#%% The Ding function, more detailed version
+
+def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1' 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2'], cols_tract=['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'pop_18_24_yr1' 'pop_18_24_yr2' 'pop_25_over_yr1', 'pop_25_over_yr2', 'pop_18_24_edu_yr1', 'pop_18_24_edu_yr2', 'pop_25_over_edu_yr1', 'pop_25_over_edu_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2'], inplace = False):
     
-    tract_vars_name = ['census_tract', 'med_rent', 'med_home_val',
-                 '18_24_pop', '25_over_pop',
-                 '18_24_edu_pop', '25_over_edu_pop',
-                 'med_hhi']
-
-    area_vars_name = ['county', 'area_med_rent', 'area_med_home_val', 'area_med_hhi']
-   
-   
-   def acs_req_tract(year):
-       
-       host = "https://api.census.gov/data"
-       dataset = "acs/acs5"
-       acs_base_url = "/".join(host, year, dataset)
-       
-       tract_vars = ['TRACT', 'B25064_001E', 'B25077_001E',
-              'S1501_C01_001E', 'S1501_C01_006E',
-              'S1501_C01_005E', 'S1501_C01_012E',
-              'B25119_001E']
-       
-       
-       predicates = {}
-       predicates["get"] = ",".join(tract_vars)
-       predicates["for"] = "tract:*"
-       predicates["in"] = "state:42, county:101"
-       #predicates["key"] = ____________________
-       res = requests.get(acs_base_url, params=predicates)
-       return res.text
-       
-# run acs tract function twice for the two user inputted years in the enclosing ding func
-# and merge
-     
-   res_1 = acs_req_tract(yr1)
-   data_1 = res_1.json()
-   df_tract_1 = pd.DataFrame(data = data_1[1:], columns = tract_vars_name )
-   
-   res_2 = acs_req_tract(yr2)
-   data_2 = res_2.json()
-   df_tract_2 = pd.DataFrame(data = data_2[1:], columns = tract_vars_name) 
-   
-   df = pd.Merge(df_tract_1, df_tract_2, on = 'census_tract', suffix = ('_1', '_2'))
-   
-#  After tract level information for both ends of study period is present and merged
-#  time to do the same for study area-wide figures.
-
-   def acs_req_area(year):
-       
-      host = "https://api.census.gov/data"
-      dataset = "acs/acs5"
-      acs_base_url = "/".join(host, year, dataset)
-      
-      area_vars = ['COUNTY', 'B25064_001E', 'B25077_001E',
-             'B25119_001E']
-      
-       predicates = {}
-       predicates["get"] = ",".join(tract_vars)
-       predicates["for"] = "county:*"
-       predicates["in"] = "state:42, county:101"
-       #predicates["key"] = ____________________
-       res = requests.get(acs_base_url, params=predicates)
-       return res.text
-   
-   area_res_1 = asc_req_area(yr1)
-   area_data_1 = area_res_1.json()
-   df_area_1 = pd.DataFrame(data = area_data_1[1:], columns = area_vars_name)
-   
-   area_res_2 = acs_req_area(yr2)
-   area_data_2 = area_res_2.json()
-   df_area_2 = pd.DataFrame(data = area_data_2[1:], columns = area_vars_name)
-   
-   area_df = pd.Merge(df_area_1, df_area_2, on = 'county', suffix = ('_1', '_2'))
-       
-       
-       
-# housekeeping
-
-   copy_df = df.copy(deep = True)
+    '''
+    Parameters
+   ----------
+    df_area: dataframe  
+        single row with columns for income variables at the county level
+        
+    df_tract: Geodataframe
+        ACS variables at the tract level for a given time period
     
-   df = df.replace("-", np.nan)
-   df = df.replace(",", "", regex= True)
-   df = df.replace("2000+", "2000")
-   df.iloc[:,2:] = df.iloc[:, 2:].astype('float')
+    cols_area: string list
+        the column names for the fields used in the area calculations for the ding function and their corresponding ACS variable name
+        0 'area_med_rent_yr1': B25064_001E
+        1 'area_med_rent_yr2' : B25064_001E
+        2 'area_med_home_val_yr1': B25077_001E
+        3 'area_med_home_val_yr2' : B25077_001E
+        4 'area_med_house_inc_yr1' : B19013_001E
+        5 'area_med_house_inc_yr2' : B19013_001E
+        
+    cols_tract: string list
+        the column names for the fields used at the tract level for the ding function and their corresponding ACS variable name
+        0.'med_rent_yr1' : B25064_001E
+        1 'med_rent_yr2': B25064_001E
+        2 'med_home_val_yr1': B25077_001E
+        3 'med_home_val_yr2': B25077_001E
+        4 'pop_18_24_yr1': S1501_C01_001E
+        5 'pop_18_24_yr2': S1501_C01_001E
+        6 'pop_25_over_yr1': S1501_C01_006E
+        7 'pop_25_over_yr2': S1501_C01_006E
+        8 'pop_18_24_edu_yr1': S1501_C01_005E
+        9 'pop_18_24_edu_yr2': S1501_C01_005E
+        10 'pop_25_over_edu_yr1': S1501_C01_012E
+        11 'pop_25_over_edu_yr2': S1501_C01_012E
+        12 'med_house_inc_yr1': B19013_001E
+        13 'med_house_inc_yr2': B19013_001E
+        
+    inplace: Boolean
+        If True returns input df with gent status and criteria columns appended,
+        If False returns just GEOID, geometry, with gent status and criteria columns
+       
+    Returns
+    --------
+    geodataframe
+    
+    
+    Notes
+    -------
+    
+    Resulting geodataframe will contain a 'gent_status' field that denotes the classification of each tract
+    the possible options:
+        0 'Weak Gentrification': tract gentrified during the time period and sits in the bottom quartile of gentrifying tracts based on home values and rents
+        1'Moderate Gentrification': tract gentrified during the time period and sits in the middle two quartiles of gentrifying tracts based on home values or rents
+        2'Intense Gentrification': tract gentrified during the time period and sits in the top quartile of gentrifying tracts based on home values or rents
+        3'No Gentrification': the tract is able to gentrified but hasn't experienced gentrification during the time period
+        4'Nongentrifiable': the tract is unable to gentrify
+       '''
+       
+# creating copies of input, used at the end for resulting DF dependent on inplace parameter choice
+
+    copy_df = df_tract.copy(deep = True)
+    df = df_tract.copy(deep=True)
+    
+# renaming columns for use in function, in case user wants to use their own
+    
+    rename_cols_tract = ['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'pop_18_24_yr1' 'pop_18_24_yr2' 'pop_25_over_yr1', 'pop_25_over_yr2', 'pop_18_24_edu_yr1', 'pop_18_24_edu_yr2', 'pop_25_over_edu_yr1', 'pop_25_over_edu_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2']
+    cols_name_dict_tract = dict(zip(cols_tract, rename_cols_tract))
+    
+    df = df.rename( columns = cols_name_dict_tract)
+    
+    rename_cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1' 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2']
+    cols_name_dict_area = dict(zip(cols_area, rename_cols_area))
+    df_area = df_area.rename(columns = cols_name_dict_area)
+    
+    #df = df.replace("-", np.nan)
+    #df = df.replace(",", "", regex= True)
+    #df = df.replace("2000+", "2000")
+    
     
 # Creating figures of total adult & adult educated population
     
     
-   df['pop_1'] = df['18_24_pop_1'] + df['25_over_pop_1'] 
-   df['pop_2'] = df['18_24_pop_2'] + df['25_over_pop_2']
+    df['pop_yr1'] = df['pop_18_24_yr1'] + df['pop_25_over_yr1'] 
+    df['pop_yr2'] = df['pop_18_24_yr2'] + df['pop_25_over_yr2']
     
-   df['edu_pop_1'] = df['18_24_edu_pop_1'] + df['25_over_edu_pop_1']
-   df['edu_pop_2'] = df['18_24_edu_pop_2'] + df['25_over_edu_pop_2']
+    df['edu_pop_yr1'] = df['pop_18_24_edu_yr1'] + df['pop_25_over_edu_yr1']
+    df['edu_pop_yr2'] = df['pop_18_24_edu_yr2'] + df['pop_25_over_edu_yr2']
     
     
     
     
 # Deriving change in share of college educated residents
     
-    df['edu_perc_1'] = (df['edu_pop_1'] / df['pop_1'])*100
-    df['edu_perc_2'] = (df['edu_pop_2'] / df['pop_2'])*100
-    chg_edu_share = df['edu_perc_2'] - df['edu_perc_1']
+    df['edu_perc_yr1'] = (df['edu_pop_yr1'] / df['pop_yr1'])*100
+    df['edu_perc_yr2'] = (df['edu_pop_yr2'] / df['pop_yr2'])*100
+    chg_edu_share = df['edu_perc_yr2'] - df['edu_perc_yr1']
     
 # deriving changes in median rents and home values per tract
     
-    chg_med_home_val = ((df['med_home_val_2'] - df['med_home_val_1']) / df['med_home_val_2'])*100
-    chg_med_rent = ((df['med_rent_2'] - df['med_rent_1']) / df['med_rent_2'])*100
+    chg_med_home_val = ((df['med_home_val_yr2'] - df['med_home_val_yr1']) / df['med_home_val_yr2'])*100
+    chg_med_rent = ((df['med_rent_yr2'] - df['med_rent_yr1']) / df['med_rent_yr2'])*100
     
 #  city wide figures
     
-    area_pop_1 = df['pop_1'].sum()
-    area_edu_pop_1 = df['edu_pop_1'].sum()
-    area_edu_share_1 = (area_edu_pop_1 / area_pop_1)*100
+    area_pop_yr1 = df['pop_yr1'].sum()
+    area_edu_pop_yr1 = df['edu_pop_yr1'].sum()
+    area_edu_share_yr1 = (area_edu_pop_yr1 / area_pop_yr1)*100
     
-    area_pop_2 = df['pop_2'].sum()
-    area_edu_pop_2 = df['edu_pop_2'].sum()
-    area_edu_share_2 = (area_edu_pop_2 / area_pop_2)*100
+    area_pop_yr2 = df['pop_yr2'].sum()
+    area_edu_pop_yr2 = df['edu_pop_yr2'].sum()
+    area_edu_share_yr2 = (area_edu_pop_yr2 / area_pop_yr2)*100
     
-    area_chg_edu_share = area_edu_share_2 - area_edu_share_1
+    area_chg_edu_share = area_edu_share_yr2 - area_edu_share_yr1
     
   
-    area_home_val_perc_inc = ((area_df['area_med_home_val_2'] - area_df['area_med_home_val_1'])/ area_df['area_med_home_val_2'])*100
-    area_med_rent_perc_inc = ((area_df['area_med_rent_2'] - area_df['area_med_rent_1'])/ area_df['area_med_rent_2'])*100
+    area_home_val_perc_inc = ((df_area['area_med_home_val_yr2'] - df_area['area_med_home_val_yr1'])/ df_area['area_med_home_val_yr2'])*100
+    area_med_rent_perc_inc = ((df_area['area_med_rent_yr2'] - df_area['area_med_rent_yr1'])/ df_area['area_med_rent_yr2'])*100
     
     #% Calculating ding index and creating a resulting dataframe with the tracts meeting the criteria
     
-    df['gentrifiable'] = np.where(df['med_hhi_1'] < area_df['area_med_hhi_1'], True, False)
+    df['gentrifiable'] = np.where(df['med_house_inc_yr1'] < df_area['area_med_house_inc_yr1'], True, False)
     
-    df['hhi_crit'] = np.where((df['gentrifiable'] == True) & (df['pop_1'] > 50), 1, 0)
+    df['hhi_crit'] = np.where((df['gentrifiable'] == True) & (df['pop_yr1'] > 50), 1, 0)
     
-    df['rent_crit'] = np.where((chg_med_rent > area_med_rent_perc_inc) & (df['pop_1'] > 50), .5, 0)
+    df['rent_crit'] = np.where((chg_med_rent > area_med_rent_perc_inc) & (df['pop_yr1'] > 50), .5, 0)
     
-    df['val_crit'] = np.where((chg_med_home_val > area_home_val_perc_inc) & (df['pop_1'] > 50), .5, 0)
+    df['val_crit'] = np.where((chg_med_home_val > area_home_val_perc_inc) & (df['pop_yr1'] > 50), .5, 0)
     
-    df['edu_crit'] = np.where((chg_edu_share > area_chg_edu_share) & (df['pop_1'] > 50), 1, 0)
+    df['edu_crit'] = np.where((chg_edu_share > area_chg_edu_share) & (df['pop_yr1'] > 50), 1, 0)
     
     
     df['crit'] = df['hhi_crit'] + df['rent_crit'] + df['val_crit'] + df['edu_crit']
     
     df['gentrifying'] = np.where(df['crit'] >= 2.5, True, False)
     
-    #%%
+# creating a gent status column that gives more detail than a binary gentrifying y/n
     
     gent_only_df = df[df['gentrifying'] == True]
     
-    quant_rent_vals = np.quantile(gent_only_df['med_rent_2'], [0, 0.25, 0.50, 0.75, 1])
+    quant_rent_vals = np.quantile(gent_only_df['med_rent_yr2'], [0, 0.25, 0.50, 0.75, 1])
     
-    quant_home_vals = np.quantile(gent_only_df['med_home_val_2'], [0, 0.25, 0.50, 0.75, 1])
+    quant_home_vals = np.quantile(gent_only_df['med_home_val_yr2'], [0, 0.25, 0.50, 0.75, 1])
     
     conditions = [
-        df['gentrifying'] == True & df['med_rent_2'] <= quant_rent_vals[1] & df['med_home_val_2'] <= quant_home_vals[1],
-        df['gentrifying'] == True & (df['med_rent_2'] <= quant_rent_vals[3] or df['med_home_vals_2'] <= quant_home_vals[3]),
-        df['gentrifying'] == True & (df['med_rent_2'] > quant_rent_vals[3] or df['med_home_vals_2'] > quant_home_vals[3]),
+        df['gentrifying'] == True & df['med_rent_yr2'] <= quant_rent_vals[1] & df['med_home_val_yr2'] <= quant_home_vals[1],
+        df['gentrifying'] == True & (df['med_rent_yr2'] <= quant_rent_vals[3] or df['med_home_val_yr2'] <= quant_home_vals[3]),
+        df['gentrifying'] == True & (df['med_rent_yr2'] > quant_rent_vals[3] or df['med_home_val_yr2'] > quant_home_vals[3]),
         df['gentrifiable'] == True & df['gentrifying'] == False,
         df['gentrifiable'] == False
         ]
@@ -186,23 +182,18 @@ def ding_calc( yr1, yr2, study_area, inplace = False):
     
     
     
-    new_cols = df[['census_tract''hhi_crit', 'rent_crit', 'val_crit', 'edu_crit', 'crit', 'gent_status']].reset_index()
+    new_cols = df[['gentrifiable','gentrifying', 'hhi_crit', 'rent_crit', 'val_crit', 'edu_crit', 'crit', 'gent_status']].reset_index()
     
-    keep_cols = df[['census_tract', 'med_rent_1', 'med_home_val_1',
-                    '18_24_pop_1', '25_over_pop_1',
-                    '18_24_edu_pop_1', '25_over_edu_pop_1',
-                    'med_hhi_1','med_rent_2', 
-                    'med_home_val_2','18_24_pop_2', 
-                    '25_over_pop_2','18_24_edu_pop_2', 
-                    '25_over_edu_pop_2']].reset_index()
+    keep_cols = df[[ 'GEOID', 'geometry']].reset_index()
     
     if inplace == False:
-        output = pd.merge(keep_cols, new_cols, on= 'census_tract')
+        output = pd.merge(keep_cols, new_cols, left_index = True, right_index = True)
     
     else:
-        output = pd.merge(copy_df, new_cols, on = 'census_tract')
+        output = pd.merge(copy_df, new_cols, left_index = True, right_index = True)
     
     return output
+
 
 
 
