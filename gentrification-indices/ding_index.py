e4ba12be7529a05af6b49a48254e3678c42d45e5
Adam Thompson <AdamTheThompson93@gmail.com>
2023-04-18 19:01:12 -0400

    updated ding index, last time?

--- gentrification-indices/ding_index.py
+++ gentrification-indices/ding_index.py
@@ -17,15 +17,9 @@ df_tract = gpd.read_file('C:/Users/tul54884/Documents/gus_8066/gentrification-in
 
 #%% The Ding function, more detailed version
 
-<<<<<<< HEAD:ding_index_.py
-<<<<<<< HEAD:ding_index.py
-def calc_ding( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1' 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2'], cols_tract=['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'pop_18_24_yr1' 'pop_18_24_yr2' 'pop_25_over_yr1', 'pop_25_over_yr2', 'pop_18_24_edu_yr1', 'pop_18_24_edu_yr2', 'pop_25_over_edu_yr1', 'pop_25_over_edu_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2'], inplace = False):
-=======
-def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1' 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2'], cols_tract=['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'tot_pop_yr1', 'tot_pop_yr2', 'bach_m_yr1', 'bach_m_yr2', 'grad_m_yr1', 'grad_m_yr2', 'bach_f_yr1', 'bach_f_yr2', 'grad_f_yr1', 'grad_f_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2'], inplace = False):
->>>>>>> 81c1f0c0870141519ef3196c671ffa3e6720d000:ding_detailed.py
-=======
-def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1' 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2'], cols_tract=['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'tot_pop_yr1', 'tot_pop_yr2', 'bach_m_yr1', 'bach_m_yr2', 'grad_m_yr1', 'grad_m_yr2', 'bach_f_yr1', 'bach_f_yr2', 'grad_f_yr1', 'grad_f_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2'], inplace = False):
->>>>>>> 81c1f0c0870141519ef3196c671ffa3e6720d000:ding_detailed.py
+
+def calc_ding( df_area, df_tract, cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1', 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2'], cols_tract=['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'pop_25_over_yr1' 'ba_degree_m_yr1', 'ma_degree_m_yr1', 'prof_degree_m_yr1', 'doc_degree_m_yr1, ba_degree_f_yr1', 'ma_degree_f_yr1', 'prof_degree_f_yr1', 'doc_degree_f_yr1', 'pop_25_over_yr2', 'ba_degree_m_yr2', 'ma_degree_m_yr2', 'prof_degree_m_yr2', 'doc_degree_m_yr2', 'ba_degree_f_yr2', 'ma_degree_f_yr2', 'prof_degree_f_yr2', 'doc_degree_f_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2'], inplace = False):
+
     
     '''
     Parameters
@@ -51,18 +45,28 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
         1 'med_rent_yr2': B25064_001E
         2 'med_home_val_yr1': B25077_001E
         3 'med_home_val_yr2': B25077_001E
-        4 'tot_pop_yr1' : C15002_001E
-        5 'tot_pop_yr2': C15002_001E
-        6 'bach_m_yr1': C15002_008E
-        7 'bach_m_yr2': C15002_008E
-        8 'grad_m_yr1': C15002_009E
-        9. 'grad_m_yr2': C15002_009E
-        10. 'bach_f_yr2': C15002_016E
-        11 'bach_f_yr2': C15002_016E
-        12. 'grad_f_yr1': C15002_017E
-        13 'grad_f_yr2': C15002_017E
-        14 'med_house_inc_yr1': B19013_001E
-        15 'med_house_inc_yr2': B19013_001E
+        4 'pop_25_over_yr1' : B15002_001E
+        5 'pop_25_over_yr2': B15002_001E
+        6 'ba_degree_m_yr1': B15002_015E
+        7 'ba_degree_m_yr2': B15002_015E
+        8 'ma_degree_m_yr1':  B15002_016E
+        9 'ma_degree_m_yr2':  B15002_016E
+        10 'prof_degree_m_yr1': B15002_017E
+        11 'prof_degree_m_yr2': B15002_017E
+        12 'doc_degree_m_yr1': B15002_018E
+        13 'doc_degree_m_yr2': B15002_018E
+        14 'prof_degree_m_yr1': B15002_017E
+        15 'prof_degree_m_yr2': B15002_017E
+        16 'ba_degree_f_yr1': B15002_032E
+        17 'ba_degree_f_yr2': B15002_032E
+        18 'ma_degree_f_yr1':  B15002_033E
+        19 'ma_degree_f_yr2':  B15002_033E
+        20 'prof_degree_f_yr1': B15002_034E
+        21 'prof_degree_f_yr2': B15002_034E
+        23 'doc_degree_f_yr1': B15002_035E
+        24 'doc_degree_f_yr2': B15002_035E
+        25 'med_house_inc_yr1': B19013_001E
+        26 'med_house_inc_yr2': B19013_001E
         
     inplace: Boolean
         If True returns input df with gent status and criteria columns appended,
@@ -92,12 +96,12 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
     
 # renaming columns for use in function, in case user wants to use their own
     
-    rename_cols_tract = ['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'tot_pop_yr1', 'tot_pop_yr2', 'bach_m_yr1', 'bach_m_yr2', 'grad_m_yr1', 'grad_m_yr2', 'bach_f_yr1', 'bach_f_yr2', 'grad_f_yr1', 'grad_f_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2']
+    rename_cols_tract = ['med_rent_yr1', 'med_rent_yr2', 'med_home_val_yr1', 'med_home_val_yr2', 'pop_25_over_yr1' 'ba_degree_m_yr1', 'ma_degree_m_yr1', 'prof_degree_m_yr1', 'doc_degree_m_yr1, ba_degree_f_yr1', 'ma_degree_f_yr1', 'prof_degree_f_yr1', 'doc_degree_f_yr1', 'pop_25_over_yr2', 'ba_degree_m_yr2', 'ma_degree_m_yr2', 'prof_degree_m_yr2', 'doc_degree_m_yr2', 'ba_degree_f_yr2', 'ma_degree_f_yr2', 'prof_degree_f_yr2', 'doc_degree_f_yr2', 'med_house_inc_yr1', 'med_house_inc_yr2']
     cols_name_dict_tract = dict(zip(cols_tract, rename_cols_tract))
     
     df = df.rename( columns = cols_name_dict_tract)
     
-    rename_cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1' 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2']
+    rename_cols_area = ['area_med_rent_yr1', 'area_med_rent_yr2', 'area_med_home_val_yr1', 'area_med_home_val_yr2', 'area_med_house_inc_yr1', 'area_med_house_inc_yr2']
     cols_name_dict_area = dict(zip(cols_area, rename_cols_area))
     df_area = df_area.rename(columns = cols_name_dict_area)
     
@@ -109,11 +113,11 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
 # Creating figures of total adult & adult educated population
     
     
-    edu_male_yr1 = df['bach_m_yr1'] + df['grad_m_yr1']
-    edu_male_yr2 = df['bach_m_yr2'] + df['grad_m_yr2']
+    edu_male_yr1 = df['ba_degree_m_yr1'] + df['ma_degree_m_yr1'] + df['prof_degree_m_yr1'] + df['doc_degree_m_yr1']
+    edu_male_yr2 = df['ba_degree_m_yr2'] + df['ma_degree_m_yr2'] + df ['prof_degree_m_yr2'] + df['doc_degree_m_yr2']
     
-    edu_female_yr1 = df['bach_f_yr1'] + df['grad_f_yr1']
-    edu_female_yr2 = df['bach_f_yr2'] + df['grad_f_yr2']
+    edu_female_yr1 = df['ba_degree_f_yr1'] + df['ma_degree_f_yr1'] + df['prof_degree_f_yr1'] + df['doc_degree_f_yr1']
+    edu_female_yr2 = df['ba_degree_f_yr2'] + df['ma_degree_f_yr2'] + df['prof_degree_f_yr2'] + df['doc_degree_f_yr2']
     
     
     
@@ -125,8 +129,8 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
     
 # Deriving change in share of college educated residents
     
-    df['edu_perc_yr1'] = (df['edu_tot_pop_yr1'] / df['tot_pop_yr1'])*100
-    df['edu_perc_yr2'] = (df['edu_tot_pop_yr2'] / df['tot_pop_yr2'])*100
+    df['edu_perc_yr1'] = (df['edu_tot_pop_yr1'] / df['pop_25_over_yr1'])*100
+    df['edu_perc_yr2'] = (df['edu_tot_pop_yr2'] / df['pop_25_over_yr2'])*100
     chg_edu_share = df['edu_perc_yr2'] - df['edu_perc_yr1']
     
 # deriving changes in median rents and home values per tract
@@ -136,13 +140,13 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
     
 #  city wide figures
     
-    area_tot_pop_yr1 = df['tot_pop_yr1'].sum()
+    area_pop_25_over_yr1 = df['pop_25_over_yr1'].sum()
     area_edu_tot_pop_yr1 = df['edu_tot_pop_yr1'].sum()
-    area_edu_share_yr1 = (area_edu_tot_pop_yr1 / area_tot_pop_yr1)*100
+    area_edu_share_yr1 = (area_edu_tot_pop_yr1 / area_pop_25_over_yr1)*100
     
-    area_tot_pop_yr2 = df['tot_pop_yr2'].sum()
+    area_pop_25_over_yr2 = df['pop_25_over_yr2'].sum()
     area_edu_tot_pop_yr2 = df['edu_tot_pop_yr2'].sum()
-    area_edu_share_yr2 = (area_edu_tot_pop_yr2 / area_tot_pop_yr2)*100
+    area_edu_share_yr2 = (area_edu_tot_pop_yr2 / area_pop_25_over_yr2)*100
     
     area_chg_edu_share = area_edu_share_yr2 - area_edu_share_yr1
     
@@ -154,13 +158,13 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
     
     df['gentrifiable'] = np.where(df['med_house_inc_yr1'] < df_area.iloc[0]['area_med_house_inc_yr1'], True, False)
     
-    df['hhi_crit'] = np.where((df['gentrifiable'] == True) & (df['tot_pop_yr1'] > 50), 1, 0)
+    df['hhi_crit'] = np.where(df['gentrifiable'] == True, 1, 0)
     
-    df['rent_crit'] = np.where((chg_med_rent > area_med_rent_perc_inc) & (df['tot_pop_yr1'] > 50), .5, 0)
+    df['rent_crit'] = np.where(chg_med_rent > area_med_rent_perc_inc, .5, 0)
     
-    df['val_crit'] = np.where((chg_med_home_val > area_home_val_perc_inc) & (df['tot_pop_yr1'] > 50), .5, 0)
+    df['val_crit'] = np.where(chg_med_home_val > area_home_val_perc_inc, .5, 0)
     
-    df['edu_crit'] = np.where((chg_edu_share > area_chg_edu_share) & (df['tot_pop_yr1'] > 50), 1, 0)
+    df['edu_crit'] = np.where(chg_edu_share > area_chg_edu_share, 1, 0)
     
     
     df['crit'] = df['hhi_crit'] + df['rent_crit'] + df['val_crit'] + df['edu_crit']
@@ -193,7 +197,7 @@ def ding_calc( df_area, df_tract, yr1, yr2, cols_area = ['area_med_rent_yr1', 'a
     
     df['gent_status'] = np.select(conditions, choices)
     
-    
+    df = df[df['pop_25_over_yr1'] > 50]
     
     
     
